27bd387c932ab0147054e881e7311820bf59c9cd
Yogi Arif Widodo <yogirenbox33@gmail.com>
2021-08-09 12:30:57 +0800

    i am rich added

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,16 @@ import 'package:flutter/material.dart';
 // the main function is the starting point for all out Flutter apps.
 void main() {
   runApp(MaterialApp(
-    home: Center(
-      child: Text("Hello World"),
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text("I am Rich"),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image.asset('images/diamond.png'),
+      ),
     ),
   ));
 }
